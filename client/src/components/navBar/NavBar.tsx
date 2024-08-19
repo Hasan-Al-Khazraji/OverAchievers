b570a444fbfaff807d826d83f9c2c947cc7c83f2
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-08-18 21:57:15 -0400

    Add Navbar UI

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import "./NavBar.css";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { Button } from "@mui/material";
+import GoogleIcon from '@mui/icons-material/Google';
 
 export default function NavBar() {
   return (
@@ -14,6 +16,16 @@ export default function NavBar() {
         <h1 className="title-text text-white font-Arcade text-lg md:text-2xl lg:text-3xl ml-2">
           OVERACHIEVERS
         </h1>
+      </div>
+      <div className="flex items-center">
+        <Tabs value={0} sx={{'& .MuiTabs-indicator': { 'backgroundColor': '#86efac' }}}>
+          <Tab label="Home" className="text-green-300 font-Retro text-lg"></Tab>
+          <Tab label="Vote" className="text-green-300 font-Retro text-lg"></Tab>
+          <Tab label="Live" className="text-green-300 font-Retro text-lg"></Tab>
+        </Tabs>
+      </div>
+      <div className="flex md:flex-grow items-center justify-end p-4">
+        <Button variant="contained" endIcon={<GoogleIcon />} className="bg-green-950">Login</Button>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "public/*.{html,js}"],
+  important: "#root",
   theme: {
     extend: {
       fontFamily:{
