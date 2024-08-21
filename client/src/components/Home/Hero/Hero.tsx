baf59288d0348f253e73b6d9659c5193d9aec61b
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-08-20 20:46:51 -0400

    Add border to hero

--- client/src/components/Home/Hero/Hero.tsx
+++ client/src/components/Home/Hero/Hero.tsx
@@ -2,10 +2,13 @@ import STLViewer from './threeD'
 
 export default function Hero() {
     return(
-        <div className="bg-gradient-to-b from-emerald-950 from-30% to-emerald-800 flex align-middle" style={{height: '80svh'}}>
-            <div className="ml-12" style={{ width: "80svh", height: "80svh" }}>
-                <STLViewer url="/trophy.STL" />
+        <>
+            <div className="bg-gradient-to-b from-emerald-950 from-30% to-emerald-800 flex align-middle" style={{height: '80svh'}}>
+                <div className="ml-12" style={{ width: "80svh", height: "80svh" }}>
+                    <STLViewer url="/trophy.STL" />
+                </div>
             </div>
-        </div>
+            <img src="/border1.png" alt="Border" className='bg-emerald-800 w-svw'/>
+        </>
     );
 }
