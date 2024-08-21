f6985a3783bbd09e6b1de88e08b8e6c5a6f7826c
Hasan-Al-Khazraji <37431365+Hasan-Al-Khazraji@users.noreply.github.com>
2024-08-20 23:08:32 -0400

    Initalize what we are component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Home/Hero/Hero";
+import WhatWeAre from "../components/Home/whatWeAre/whatWeAre";
 
 export default function Home() {
     return (
       <>
         <Hero />
+        <WhatWeAre />
       </>
     );
   }
